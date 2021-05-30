1be428afbe183a7dafe5adb4fc59e4215798093d
Oluf <mkp244@alumni.ku.dk>
2021-05-30 13:19:56 +0200

    Making a mess of it

--- OptimalPF/pftools.py
+++ OptimalPF/pftools.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas_datareader.data as web
 
 ##################
 ## data section ##
@@ -16,15 +17,78 @@ def csv_extractor(ticker,apath):
     temp_df = temp_df.set_index('Date', drop = True)
     return temp_df
 
+# def df_generator_csv(tickers, apath):
+#     # i. temporarily storing each df in a list
+#     temp_store = []
+    
+#     # ii. creating a df for each ticker
+#     for i in tickers:
+#         vector = csv_extractor(i, apath)
+#         temp_store.append(vector)
+    
+#     # iii. combining the dfs
+#     df = pd.concat(temp_store, axis = 1)
+#     df = df.fillna(method = 'ffill')
+    
+#     # iv. creating a second df with pct.-returns
+#     rdf = (df/df.shift(1)-1)*100
+    
+#     # v. creating a third df with cumulativ return
+#     cdf = (df/df.shift(1)-1)
+#     for col in cdf.columns:
+#         cdf[col] = np.cumprod(1+cdf[col])*100
+    
+#     return df, rdf, cdf
+
+def yahoo_extractor(ticker, start, end):
+    temp_df = web.DataReader(ticker, 'yahoo', start, end)
+    # temp_df = pd.DataFrame(temp)
+    temp_df = temp_df['Adj Close']
+    temp_df[ticker] = temp_df
+    # temp_df.rename(columns = {'Adj Close': ticker})
+    # temp_df = temp_df.rename(columns = {'Adj Close': ticker})
+    return temp_df
 
 
-def df_generator_csv(tickers, apath):
+# def df_generator_yahoo(tickers, start, end):
+#     # i. temporarily storing each df in a list
+#     temp_store = []
+    
+#     # ii. creating a df for each ticker
+#     for i in tickers:
+#         vector = yahoo_extractor(i, start, end)
+#         temp_store.append(vector)
+    
+#     # iii. combining the dfs
+#     df = pd.concat(temp_store, axis = 1)
+#     df = df.fillna(method = 'ffill')
+    
+#     # iv. creating a second df with pct.-returns
+#     rdf = (df/df.shift(1)-1)*100
+    
+#     # v. creating a third df with cumulativ return
+#     cdf = (df/df.shift(1)-1)
+#     for col in cdf.columns:
+#         cdf[col] = np.cumprod(1+cdf[col])*100
+    
+#     return df, rdf, cdf
+
+
+def df_generator(tickers, method, start = None, end = None, apath = None):
     # i. temporarily storing each df in a list
     temp_store = []
     
     # ii. creating a df for each ticker
     for i in tickers:
-        vector = csv_extractor(i, apath)
+        if method == 'yahoo':
+            # assert start == None, 'please denote a start-time (dt-format) when extracting data'
+            # assert end == None, 'please denote a end-time (dt-format) when extracting data'
+            vector = yahoo_extractor(i, start, end)
+        elif method == 'csv':
+            assert apath == None, 'please denote an absolute path to your local csv-files'
+            vector = csv_extractor(i, apath)
+        else:
+            print('did not recognize method, please use either yahoo or csv')
         temp_store.append(vector)
     
     # iii. combining the dfs
@@ -39,34 +103,38 @@ def df_generator_csv(tickers, apath):
     for col in cdf.columns:
         cdf[col] = np.cumprod(1+cdf[col])*100
     
+    # ii. describes data
+    des_df(df)
+
     return df, rdf, cdf
 
 
-def df_generator_yahoo(tickers):
-    print('Yahoo dataextractor will be implemented later')
+
+# def df_generator_yahoo(tickers):
+#     print('Yahoo dataextractor will be implemented later')
 
 
 def des_df(dfs):
     print('Will eventually output description of each ticker in df')
 
 
-def df_generator(tickers, method = 'Yahoo', apath = None):
+# def df_generator(tickers, method = 'yahoo', apath = None):
     
-    # i. extracts data
-    if method == 'Yahoo':
-        df, rdf, cdf = df_generator_yahoo(tickers)
+#     # i. extracts data
+#     if method == 'yahoo':
+#         df, rdf, cdf = df_generator_yahoo(tickers)
     
-    elif method == 'csv':
-        assert len(apath)>2, 'when using csv-files the absolute path of the folder in which data lies, must be specified'
-        df, rdf, cdf = df_generator_csv(tickers, apath)
+#     elif method == 'csv':
+#         assert len(apath)>2, 'when using csv-files the absolute path of the folder in which data lies, must be specified'
+#         df, rdf, cdf = df_generator_csv(tickers, apath)
         
-    else:
-        print('doesnt recognize method')
+#     else:
+#         print('doesnt recognize method')
     
-    # ii. describes data
-    des_df(df)
+#     # ii. describes data
+#     des_df(df)
     
-    return df, rdf, cdf
+#     return df, rdf, cdf
 
 
 
