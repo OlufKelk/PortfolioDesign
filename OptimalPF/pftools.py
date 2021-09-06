b6ad2654c63fca0108d22269a0adb1565c829e19
Oluf <mkp244@alumni.ku.dk>
2021-09-06 17:49:40 +0200

    Analytical and numerical solution to minimum variance and efficient tangent pf - made functions

--- OptimalPF/pftools.py
+++ OptimalPF/pftools.py
@@ -164,12 +164,82 @@ def desc_ticks(data,rdata,cdata,rfree=0):
 ####################################################
 ## 1. analytical solution (short-selling allowed) ##
 ####################################################
-# take the solutions from notebook
+# Analytical solution for minimum variance portfolio
+def mvar_analytical(sigma,mu,printres = True):
+    # i. calculating the optimal weights for a minimum variance portfolio
+    weights = np.linalg.inv(sigma)@np.ones((len(sigma.columns)))
+    weights = weights / sum(weights)
+    
+    # ii. calculating variance, standard deviation and expected return
+    variance = weights@sigma@np.transpose(weights)
+    sd = np.sqrt(variance)
+    pfreturn = weights@mu
+    
+    # iii. creating dataframe containing weights and another with its stats  
+    wopt = weights*100
+    wopt = [round(w,2) for w in wopt]
+    names = sigma.columns
+    wpfdf = pd.DataFrame({'ticker':names, 'weight':wopt})
+    wpfdf = wpfdf.set_index('ticker')
+    statspfdf = pd.DataFrame.from_dict({'variance': round(variance,2), 'std': round(sd,2),
+                                       'return': round(pfreturn,2), 'sharpe-ratio':round(pfreturn/sd,2)},
+                                       orient = 'index', columns = ['stats'])
+    
+    # iv. printing statement 
+    if printres:
+            print(f'-----------------------------------------------------------------------------------------------')
+            print(f'\nThe analytical solution for the minimum variance portfolio (allows for shorting) resulted in:\n')
+            print(f'{wpfdf}\n')
+            print(f'With portfolio characteristics:\n')
+            print(f'{statspfdf}\n')
+            print(f'-----------------------------------------------------------------------------------------------')
+    
+    return wpfdf, statspfdf
 
 
+# Analytical solution for the efficient tangent portfolio
+def tan_analytical(sigma,mu,printres = True):
+    # i. calculating the optimal weights for the efficient tangent portfolio
+    weights = np.linalg.inv(sigma) @ mu
+    weights = weights / sum(weights)
+    
+    # ii. calculating variance, standard deviation and expected return
+    variance = weights@sigma@np.transpose(weights)
+    sd = np.sqrt(variance)
+    pfreturn = weights@mu
+    
+    # iii. creating dataframe containing weights and another with its stats  
+    wopt = weights*100
+    wopt = [round(w,2) for w in wopt]
+    names = sigma.columns
+    wpfdf = pd.DataFrame({'ticker':names, 'weight':wopt})
+    wpfdf = wpfdf.set_index('ticker')
+    statspfdf = pd.DataFrame.from_dict({'variance': round(variance,2), 'std': round(sd,2),
+                                       'return': round(pfreturn,2), 'sharpe-ratio':round(pfreturn/sd,2)},
+                                       orient = 'index', columns = ['stats'])
+    
+    # iv. printing statement 
+    if printres:
+            print(f'-----------------------------------------------------------------------------------------------')
+            print(f'\nThe analytical solution for the efficient tangent portfolio (allows for shorting) resulted in:\n')
+            print(f'{wpfdf}\n')
+            print(f'With portfolio characteristics:\n')
+            print(f'{statspfdf}\n')
+            print(f'-----------------------------------------------------------------------------------------------')
+    
+    return wpfdf, statspfdf
 
 
-
+def ana_optimal_portfolios(df):
+    # i. calculating relevant data for optimal portfolios
+    logrdf = np.log(df).diff().dropna()*100
+    sigma = logrdf.cov()*252
+    mu = logrdf.mean()*252
+    
+    # ii. calling analytical optimizers
+    wmvdf,statmvdf = mvar_analytical(sigma,mu)
+    wtandf,stattandf = tan_analytical(sigma,mu)
+    
 
 
 ######################################################
@@ -204,111 +274,47 @@ def tangent(w,sigma,mu):
     return sharperatio
 
 
-
-
-
-
-# Uses multistart to alleviate problem with local minima (due to non-convexities)
-def opt_mvar(df,N=50):
-    # i. preparing data
-    logrdf = np.log(df).diff().dropna()*100
-    # calculating covariance matrix -> from daily to yearly
-    sigma = logrdf.cov()*252
-    
-    # ii. initiating
-    names = df.columns
-    M = len(names)
-    w0s = np.random.uniform(1e-8,1-1e-8,size = (N,M))
-    ws = np.empty((N,M))
-    fs = np.empty(N)
-    # optimal variance + its weights
-    fopt = np.inf
-    wopt = np.nan
-    
-    
-    # iii. objective functions
-    obj = lambda x: mvar(x,sigma)
-
-    # iv. bounds, weights cannot be negative, i.e. short-selling is not allowed
-    bound = (1e-8,1-1e-8)
-    bounds = ((bound, ) * M)
-    
-    # v. multistart using SLSQP (bounded) minimizer
-    for i, w0 in enumerate(w0s):
-        # a. bounded optimization for given initial weights
-        result = optimize.minimize(obj,w0,method = 'SLSQP',
-                                  bounds=bounds)
-        
-        # b. storing solution (variance + its weights)
-        ws[i,:] = result.x
-        f = result.fun
-
-        # c. printing first 5 optimizations or if better than previously seen
-        if i < 5 or f < fopt:
-            # 1. normalizing and making presentable
-            w0 = [round(w,2) for w in w0]
-            weights = ws[i]/sum(ws[i])*100 
-            weights = [round(w,2) for w in weights]
-            
-            # 2. storing optimal value
-            if f < fopt:
-                fopt = f
-                wopt = weights
-            
-            # 3. print statement
-            print(f'Attempt {i+1}: w0 (initial guess) = {w0}\n')
-            print(f'Weights converged at {weights} with variance = {f:.2f}\n')
-            print(f'----------------------------------------------------------------------')
-    
-    # vi. saving weights and ticker name
-    mvardf = pd.DataFrame({'ticker':names, 'weight':wopt})
-    mvardf = mvardf.set_index('ticker')
-    
-    
-    # vii. printing best solution
-    print(f'\nThe minimum variance portfolio from {N} total attemps resulted in:\n{mvardf} \nWith variance = {fopt:.2f}')
-    print(f'----------------------')
-    
-    return wopt, fopt
-
-
-
-def optimal_portfolios(df,printres = True,N=50):
+def num_optimal_portfolios(df,N=50,shorting = False):
     # i. preparing data
     logrdf = np.log(df).diff().dropna()*100
     sigma = logrdf.cov()*252
     mu = logrdf.mean()*252
-    names = df.columns
+    
+    # ii. Stating whether shorting is allowed or not
+    if shorting:
+        print(f'SHORTING IS ALLOWED')
+    else:
+        print(f'SHORTING IS NOT ALLOWED')
+    
     
     # ii. storing results for each of the portfolios
-    mvwdf, mwstatsdf = optimize_pf('minvar',sigma,mu,N)
-    twdf, tstatsdf = optimize_pf('tangent',sigma,mu,N)
-#     cw, cv, cr,  tvsr, tvcr = optimize_pf('calmar',sigma,N)
+    mvwdf, mwstatsdf = optimize_pf('minvar',sigma,mu,N,shorting)
+    twdf, tstatsdf = optimize_pf('tangent',sigma,mu,N,shorting)
+    # cw, cv, cr,  tvsr, tvcr = optimize_pf('calmar',sigma,N) # will be developed eventually?
     
-    # iii. printing statement
-    if printres:
-        print('some print statement')
-    
-    # iv. creating dataframe based on the store results
-    # create some sort of overview using dataframe above
-#     optimal_portfolios = pd.DataFrame({}) 
-
-    return optimal_portfolios
 
 
 
 
-def optimize_pf(pftype,sigma,mu,N):
+def optimize_pf(pftype,sigma,mu,N,shorting = False):
     # i. initiating
     names = sigma.columns
     M = len(names)
-    w0s = np.random.uniform(1e-8,1-1e-8,size = (N,M))
     ws = np.empty((N,M))
     fs = np.empty(N) # 
     fopt = np.inf # initialize optimal value
     wopt = np.nan # initialize optimal weights
-    # iv. bounds, weights cannot be negative, i.e. short-selling is not allowed
-    bound = (1e-8,1-1e-8)
+    
+    # iv. creating bounds based on whether short-selling is allowed or not
+    np.random.seed(1986)
+    if shorting == True:
+        w0s = np.random.uniform(-1+1e-8,1-1e-8,size = (N,M))
+        bound = (-1+1e-8,1-1e-8)
+    else:
+        # bounds, weights cannot be negative, i.e. short-selling is not allowed
+        np.random.seed(1986)
+        w0s = np.random.uniform(1e-8,1-1e-8,size = (N,M))
+        bound = (1e-8,1-1e-8)
     bounds = ((bound, ) * M)
 
     # iii. objective functions
@@ -328,7 +334,7 @@ def optimize_pf(pftype,sigma,mu,N):
     else:
         print(f'Can only optimize portfolios: minimum variance (pftype = minvar), efficient tangent (pftype = tangent) and calmar ratio (pftype = calmar)')
     
-    
+    print(f'\nMultistart optimizing - prints everytime the optimal solution improves ')
     print(f'-----------------------------------------------------------------------------------------------')
     # v. multistart using SLSQP (bounded) minimizer
     for i, w0 in enumerate(w0s):
@@ -341,7 +347,7 @@ def optimize_pf(pftype,sigma,mu,N):
         f = result.fun
 
         # c. printing first 5 optimizations or if better than previously seen
-        if i < 5 or f < fopt:
+        if i < 1 or f < fopt:
             # 1. normalizing
             weights = ws[i,:]/sum(ws[i,:])
             
@@ -360,23 +366,18 @@ def optimize_pf(pftype,sigma,mu,N):
             else:
                 f = -f
             # 4. print statement
-            print(f'Attempt {i+1} - {pftype} portfolio - with w0 (initial guess) = {w0}\n')
+            print(f'Attempt {i+1} of {N} - {pftype} portfolio - with w0 (initial guess) = {w0}\n')
             print(f'Weights converged at {weights} with {optimizing} = {f:.2f}.\n\n')
             
     
     if pftype == 'minvar':
         vopt = fopt
         sropt = ropt/np.sqrt(vopt)
-#         cropt = XX
     elif pftype == 'tangent':
         vopt = wopt@sigma@wopt
         sropt = -fopt
-#         cropt = XX
     else:
         print('will be done')
-#         vopt = wopt@sigma@wopt
-#         sropt = ropt/np.sqrt(vopt)
-#         cropt = -fopt
     
     # vi. saving weights and portfolio stats
     nwopt = wopt*100
@@ -417,15 +418,12 @@ def optimize_pf(pftype,sigma,mu,N):
 
 
 # optimal portfolio
-## analytical solution (mutual funds theorem)
 ## simulated results (in parallel)
 ## Get stats for the portfolio
 
 
 # include in stats: 
 ## Stats for how much of a normal distribution it is 
-## Value at Risk (see financial econometrics), both normally distributed and skewed
-## Expected Shortfall (see financial econometrics), both normally distributed and skewed
 
 # inlcude in plots:
 ## Simulated path
@@ -443,79 +441,3 @@ def optimize_pf(pftype,sigma,mu,N):
 ###############
 ## graveyard ##
 ###############
-# def df_generator_csv(tickers, apath):
-#     # i. temporarily storing each df in a list
-#     temp_store = []
-    
-#     # ii. creating a df for each ticker
-#     for i in tickers:
-#         vector = csv_extractor(i, apath)
-#         temp_store.append(vector)
-    
-#     # iii. combining the dfs
-#     df = pd.concat(temp_store, axis = 1)
-#     df = df.fillna(method = 'ffill')
-    
-#     # iv. creating a second df with pct.-returns
-#     rdf = (df/df.shift(1)-1)*100
-    
-#     # v. creating a third df with cumulativ return
-#     cdf = (df/df.shift(1)-1)
-#     for col in cdf.columns:
-#         cdf[col] = np.cumprod(1+cdf[col])*100
-    
-#     return df, rdf, cdf
-# def df_generator(tickers, method = 'yahoo', apath = None):
-    
-#     # i. extracts data
-#     if method == 'yahoo':
-#         df, rdf, cdf = df_generator_yahoo(tickers)
-    
-#     elif method == 'csv':
-#         assert len(apath)>2, 'when using csv-files the absolute path of the folder in which data lies, must be specified'
-#         df, rdf, cdf = df_generator_csv(tickers, apath)
-        
-#     else:
-#         print('doesnt recognize method')
-    
-#     # ii. describes data
-#     des_df(df)
-    
-#     return df, rdf, cdf
-
-# def df_generator_yahoo(tickers, start, end):
-#     # i. temporarily storing each df in a list
-#     temp_store = []
-    
-#     # ii. creating a df for each ticker
-#     for i in tickers:
-#         vector = yahoo_extractor(i, start, end)
-#         temp_store.append(vector)
-    
-#     # iii. combining the dfs
-#     df = pd.concat(temp_store, axis = 1)
-#     df = df.fillna(method = 'ffill')
-    
-#     # iv. creating a second df with pct.-returns
-#     rdf = (df/df.shift(1)-1)*100
-    
-#     # v. creating a third df with cumulativ return
-#     cdf = (df/df.shift(1)-1)
-#     for col in cdf.columns:
-#         cdf[col] = np.cumprod(1+cdf[col])*100
-    
-#     return df, rdf, cdf
-
-# def df_generator_yahoo(tickers):
-#     print('Yahoo dataextractor will be implemented later')
-
-
-
-
-
-
-
-
-
-
-
